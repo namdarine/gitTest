4000314688a6e0d0a416dad55a741d4f71875fa4
Nam <namdarine.code@gmail.com>
2022-01-19 20:32:47 +0900

    Please this is works after generate

--- gitTest/src/gitTest/helloWorld.java
+++ gitTest/src/gitTest/helloWorld.java
@@ -6,6 +6,7 @@ public class helloWorld {
 		System.out.println("Hello, World!");
 		System.out.println("This is push test to github.");
 		System.out.println("Trying to get used to it.");
+		System.out.println("Please, it works");
 
 	}
 
